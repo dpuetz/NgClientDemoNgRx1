5af4948f53d8aeec2b32a9bca6a47b209b453b47
dp <me@gmail.com>
2018-08-10 09:23:03 -0700

    create app-routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-// import { FormsModule } from '@angular/forms';
-// import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';  //https://fontawesome.com/how-to-use/svg-with-js
-import { RouterModule, PreloadAllModules } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { LoginModule } from './login/login.module';
@@ -17,33 +13,7 @@ import { PageNotFoundComponent } from './home/page-not-found.component';
 import { ShellComponent } from './home/shell.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { AdminModule } from './admin/admin.module';
-// import { LoginComponent } from './login/login.component';
-// import { MessageComponent } from './shared/message.component';
-
-// import { CreateAccountComponent } from './user/create-account.component';
-
-const appRoutes  = [
-    {
-        path: '',
-        component: ShellComponent,
-        children: [
-                { path: 'welcome', component: WelcomeComponent },
-                {
-                    path: 'websites',
-                    loadChildren: './websites/websites.module#WebsitesModule'
-                },
-                // { path: 'login', component: LoginComponent },
-                // { path: 'create', component: CreateAccountComponent },
-                { path: 'welcome', component: WelcomeComponent },
-                // {
-                //     path: 'user',
-                //     loadChildren: './user/user.module#UserModule'
-                // },
-                { path: '', redirectTo: '/welcome', pathMatch: 'full'},
-        ]
-    },
-     { path: '**', component: PageNotFoundComponent }
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
@@ -51,8 +21,6 @@ const appRoutes  = [
     AngularFontAwesomeModule,
     HttpClientModule,
     SharedModule,
-    // FormsModule,
-    // ReactiveFormsModule,
     EffectsModule.forRoot([]),
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
@@ -60,17 +28,14 @@ const appRoutes  = [
     }),
     LoginModule,
     AdminModule,
-    RouterModule.forRoot(appRoutes,  {preloadingStrategy: PreloadAllModules} ),
-  ],
+    AppRoutingModule
+    ],
   declarations: [
     AppComponent,
     MenuComponent,
     PageNotFoundComponent,
     ShellComponent,
-    WelcomeComponent,
-    // LoginComponent,
-    // MessageComponent
-
+    WelcomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
