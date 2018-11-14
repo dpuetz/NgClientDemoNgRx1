9656f39ad9cadb7bc146f8d73c91bd84ff284843
dp <me@gmail.com>
2018-11-14 10:59:12 -0800

    set correct environment path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "http://localhost/NgApiDemo/"
-//   baseUrl: "http://takeMeToYourData.com/NgApiDemo/NgApiDemo/"
+  baseUrl: "http://takeMeToYourData.com/NgApiNgRxDemo/NgApiDemo/"
 };
